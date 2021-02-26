1f06926304fe114edf1ab34ce02492f81a9bc7db
brunosmoura <brunosmoura@outlook.com>
2021-02-26 18:22:47 -0300

    primeiro

---
+++ Pessoa.php
@@ -0,0 +1,15 @@
+<?php
+
+class Pessoa {
+private $nome;
+
+public function __
+
+public function setNome($novoNome){
+    $this->nome = $novoNome;
+}
+
+public function getNome(){
+return $this->nome;
+    }
+}
